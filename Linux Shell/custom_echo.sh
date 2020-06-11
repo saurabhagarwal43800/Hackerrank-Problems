4e72a5280955a6bcb3a072370fbd7254c6f33a96
Saurabh Agarwal <33370952+saurabhagarwal43800@users.noreply.github.com>
2020-06-11 23:07:48 +0530

    Basic echo

---
+++ Linux Shell/custom_echo.sh
@@ -0,0 +1,25 @@
+# Write a Bash script which accepts name as input and displays a greeting: "Welcome (name)"
+
+# Input Format:
+
+# One line, containing a .
+
+# Output Format
+
+# One line: "Welcome (name)" (quotation marks excluded).
+# The evaluation will be case-sensitive.
+
+# Sample Input:
+# Dan 
+ 
+# Sample Output:
+# Welcome Dan
+
+#---------------------------------------------------------------------
+
+# Main Code:
+
+read name
+echo Welcome $name
+
+#---------------------------------------------------------------------
---
+++ Linux Shell/echo.sh
@@ -0,0 +1,15 @@
+# Write a bash script which does just one thing: saying "HELLO".
+
+# Input Format:
+# There is no input file required for this problem.
+
+# Output Format:
+# HELLO
+
+#---------------------------------------------------------------------
+
+# Main Code:
+
+echo HELLO
+
+#---------------------------------------------------------------------
